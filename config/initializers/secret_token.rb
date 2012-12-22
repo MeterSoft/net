0f5d28ce0ddf16f0717eff2a0e3c72b1261e2d8e
MeterSoft <voinbpy@mail.ru>
2012-12-22 12:48:12 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Net::Application.config.secret_token = 'e4d104d65f763f056c331c314d3c11ae89af4d703ff3322602f45b25a7aad6ae1e366b5a4193f10a9837d44952046ee7feee49f0c7a72f7edbad73d4e10bd65f'
