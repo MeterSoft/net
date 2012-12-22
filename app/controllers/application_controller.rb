b6c65267e06b2cdf651e477c286cc24094822ef0
MeterSoft <voinbpy@mail.ru>
2012-12-22 19:56:57 +0200

    seting devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+
   before_filter :authenticate_user!
 
-  protect_from_forgery
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
 end
